844a7c7a9f68e979aaf9860a302a2efaac478471
dinasalsabilla <dinasalsabilla05@gmail.com>
2025-04-13 22:39:05 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022300153/CovidConfig.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace tpmodul8_103022300153
+{
+    class CovidConfig
+    {
+        private const string ConfigFileName = "covid_config.json";
+        public string satuan_suhu { get; set; } = "celcius";
+        public int batas_hari_deman { get; set; } = 14;
+        public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        public static CovidConfig LoadConfig()
+        {
+            if (File.Exists(ConfigFileName))
+            {
+                string json = File.ReadAllText(ConfigFileName);
+                return JsonConvert.DeserializeObject<CovidConfig>(json);
+            }
+            else
+            {
+                CovidConfig defaultConfig = new CovidConfig();
+                defaultConfig.SaveConfig();
+                return defaultConfig;
+            }
+        }
+
+        public void SaveConfig()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(ConfigFileName, json);
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu.ToLower() == "celcius")
+            {
+                satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                satuan_suhu = "celcius";
+            }
+            SaveConfig();
+        }
+    }
+}
--- tpmodul8_103022300153/Program.cs
+++ tpmodul8_103022300153/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace tpmodul8_103022300153
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CovidConfig config = CovidConfig.LoadConfig();
+
+            config.UbahSatuan();
+
+            Console.Write("Berapa suhu badan anda saat ini? Dalam nilai " + config.satuan_suhu + ": ");
+            double suhu = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+            int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+            bool suhuNormal = false;
+            if (config.satuan_suhu.ToLower() == "celcius")
+            {
+                suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+            }
+            else if (config.satuan_suhu.ToLower() == "fahrenheit")
+            {
+                suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+            }
+
+            bool hariValid = hariDemam < config.batas_hari_deman;
+            if (suhuNormal && hariValid)
+            {
+                Console.WriteLine(config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(config.pesan_ditolak);
+            }
         }
     }
 }
